ba2ca4116caae7699f3570e52c91c2ba24f9fedd
JackCannon92 <martinsirizzotti@gmail.com>
2026-05-16 11:36:24 -0300

    Add files via upload

--- routes/estudiantesRoutes.js
+++ routes/estudiantesRoutes.js
@@ -5,7 +5,7 @@ const router = express.Router();
 
 // Importamos el controlador y el NUEVO validador
 const estudiantesController = require('../controllers/estudiantesController');
-const estudiantesValidator = require('../validators/estudiantesValidators');
+const estudiantesValidator = require('../validators/estudiantesValidator');
 
 // 1. BROWSE
 router.get('/', estudiantesController.obtenerTodos);
@@ -13,10 +13,10 @@ router.get('/', estudiantesController.obtenerTodos);
 // 2. READ
 router.get('/:id', estudiantesController.obtenerPorId);
 
-// 3. ADD (Aquí ponemos al guardia en el medio)
+// 3. ADD 
 router.post('/', estudiantesValidator.validarDatosEstudiante, estudiantesController.crear);
 
-// 4. EDIT (Aquí también ponemos al guardia en el medio)
+// 4. EDIT
 router.put('/:id', estudiantesValidator.validarDatosEstudiante, estudiantesController.actualizar);
 
 // 5. DELETE
